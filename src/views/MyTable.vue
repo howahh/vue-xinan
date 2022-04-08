fe75f21b2b56c8fdbd6d92e5e1eb501bdc3b7fc1
howahh <871759032@qq.com>
2022-04-08 09:23:42 +0800

    new

---
+++ src/views/MyTable.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="all">
+        <el-row :gutter="4">
+            <el-col :span="6">
+                <el-row>1</el-row>
+                <el-row>2</el-row>
+            </el-col>
+            <el-col>
+                <el-row>3</el-row>
+                <el-row>4</el-row>
+            </el-col>
+        </el-row>
+        <el-row :gutter="4">
+
+        </el-row>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
